57fc3cc679a5a0fa9341b1b7455ba3832c85a615
gealsanchez <gealsanchez@gmail.com>
2023-02-10 12:11:24 -0500

    Shorthand ESLint updated

--- src/modules/postComment.js
+++ src/modules/postComment.js
@@ -4,9 +4,12 @@ const postComment = async (name, comment, idMeal) => {
   const response = await fetch(URL, {
     method: 'POST',
     body: JSON.stringify({
-      idMeal,
-      name,
-      comment,
+      // eslint-disable-next-line
+      item_id: idMeal,
+      // eslint-disable-next-line
+      username: name,
+      // eslint-disable-next-line
+      comment: comment,
     }),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
