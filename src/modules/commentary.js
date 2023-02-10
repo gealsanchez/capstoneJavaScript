e5a18f3195178de18382ba868723fe5d9ad4f2c2
gealsanchez <gealsanchez@gmail.com>
2023-02-10 11:23:18 -0500

    index.html updated

---
+++ src/modules/commentary.js
@@ -0,0 +1,18 @@
+import getComments from './getComments.js';
+import commentCard from './commentCard.js';
+import commentCounter from './commentCounter.js';
+
+const commentary = async (idMeal) => {
+  const counter = document.querySelector('.divCounter');
+  const div = document.createElement('div');
+  div.className = 'counting';
+  const data = await getComments(idMeal);
+  data.forEach((comment) => {
+    commentCard(comment.username, comment.creation_date, comment.comment);
+  });
+  counter.innerHTML = '';
+  div.innerHTML = `<h4></h4><h4>Comments(${commentCounter()})</h4>`;
+  counter.appendChild(div);
+};
+
+export default commentary;
---
+++ src/test/capstonejavascript.test.js
@@ -0,0 +1,15 @@
+import commentCounter from '../modules/commentCounter.js';
+import mealCounter from '../modules/mealCounter.js';
+
+describe('Test methods', () => {
+  test('Counting comments', () => {
+    document.body.innerHTML = '<div class="comment"></div><div class="comment"></div><div class="comment"></div>';
+    const comments = commentCounter();
+    expect(comments).toBe(3);
+  });
+  test('Counting meals', () => {
+    document.body.innerHTML = '<div class="mealItem"></div><div class="mealItem"></div><div id="meals-counter"></div>';
+    const meals = mealCounter();
+    expect(meals).toBe(2);
+  });
+});
